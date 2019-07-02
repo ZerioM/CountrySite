36f6263268fa21a220380f366e659cbc566707b7
MarioZeller <mario.zeller98@gmx.at>
2019-07-02 21:34:22 +0200

    Update Starter.cs
    The Business Skeleton of Starter.cs was missing.

--- BL_CountrySite/Starter.cs
+++ BL_CountrySite/Starter.cs
@@ -6,8 +6,28 @@ using System.Threading.Tasks;
 
 namespace BL_CountrySite
 {
-    public class Starter
+    public static class Starter
     {
         //dies ist der Work-Branch Tag 1
+        public static Posts getAllPosts() {
+            return null;
+        }
+
+        public static Countries getAllCountries() {
+            return null;
+        }
+
+        public static Transports getAllTransports() {
+            return null;
+        }
+
+        public static object searchByName(string name) {
+            return null; 
+        }
+
+        public static loggedInUser login(string username, string password) {
+            return null;
+        }
+
     }
 }
